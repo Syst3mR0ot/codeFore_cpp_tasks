acf132eaff7a7f85b8c08f3ed20f83874df41782
Syst3mR0ot <ahmedsgk@gmail.com>
2023-05-01 08:54:37 +0300

    add final

--- src/loop_W.cpp
+++ src/loop_W.cpp
@@ -3,11 +3,22 @@
 using namespace std;
 
 int main(){
+
     int count;
     cin >> count;
-    
-
     for(int i=0; i <= count; i++){
+        for(int k=0; k<count-i; k++){
+            cout << " ";
+        }
+        for(int j=0; j <(i*2)-1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=count; i > 0; i--){
+        for(int k=0; k<count-i; k++){
+            cout << " ";
+        }
         for(int j=0; j <(i*2)-1; j++){
             cout << "*";
         }
